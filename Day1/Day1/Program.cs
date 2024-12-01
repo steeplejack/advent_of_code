815baab47476d0cbe9807ac5ab71adce029eb357
steeplejack <kcgori1982@gmail.com>
2024-12-01 12:03:09 +0000

    Solved Day1 part 1 in C#

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text.RegularExpressions;
+struct Inputs
+{
+    public int[] left;
+    public int[] right;
 
-Console.WriteLine("Hello, World!");
+    public Inputs(int[] left, int[] right)
+    {
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Program
+{
+    static int sum_of_sorted_differences(in int[] left, in int[] right)
+    {
+        int[] x = (int[])left.Clone();
+        int[] y = (int[])right.Clone();
+
+        Array.Sort(x);
+        Array.Sort(y);
+
+        var zip = x.Zip(y, (a, b) => Math.Abs(b - a));
+
+        var sum = 0;
+        foreach (var diff in zip)
+        {
+            sum += diff;
+        }
+
+        return sum;
+    }
+
+    static Inputs read_input_file(in string filename)
+    {
+        var left = new List<int>();
+        var right = new List<int>();
+
+        using (StreamReader reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var values = Regex.Split(line, @"\s+");
+                left.Add(int.Parse(values[0]));
+                right.Add(int.Parse(values[1]));
+            }
+        }
+
+        return new Inputs(left.ToArray(), right.ToArray());
+    }
+    static void Main(string[] args)
+    {
+        var inputs = read_input_file(args[0]);
+        int result = sum_of_sorted_differences(inputs.left, inputs.right);
+        Console.WriteLine($"{result}");
+    }
+}
