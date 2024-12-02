c7e6bf836eed911a716f995540294ea19b030cd8
steeplejack <kcgori1982@gmail.com>
2024-12-02 13:08:21 +0000

    Solves Day2 part 1

---
+++ Day2/Day2/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    static int[] Diff(int[] input)
+    {
+        return input
+            .SkipLast(1)
+            .Zip(input.Skip(1),
+                (f, l) => l - f)
+            .ToArray();
+    }
+
+    static bool IsSafe(int[] input)
+    {
+        var diff = Diff(input);
+        var sign = Math.Sign(diff[0]);
+        return diff.All(i =>
+        { 
+            int abs = Math.Abs(i);
+            return (Math.Sign(i) == sign) && abs > 0 && abs < 4;
+        });
+    }
+    
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new StreamReader(args[0]);
+        string line;
+        int s = 0;
+        while ((line = reader.ReadLine()) != null)
+        {
+            var numbers = line.Trim()
+                .Split(' ')
+                .Select(a => int.Parse(a))
+                .ToArray();
+            if (IsSafe(numbers))
+            {
+                s += 1;
+            }
+        }
+        Console.WriteLine(s);
+    }
+}
