174ad151a59867a02b2eec29ca0aac812ac9b0bd
steeplejack <kcgori1982@gmail.com>
2024-12-03 09:55:51 +0000

    Part 1 Works on example string

---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day3;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Regex mulRegex = new Regex(@"(mul\((\d+),(\d+)\))", RegexOptions.Compiled);
+        var example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))\n";
+        var matches = mulRegex.Matches(example);
+        int s = 0;
+        foreach (Match rgx in matches)
+        {
+            int a = int.Parse(rgx.Groups[2].Value);
+            int b = int.Parse(rgx.Groups[3].Value);
+            s += a * b;
+        }
+        Console.WriteLine(s);
+    }
+}
