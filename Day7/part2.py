b93935f156c6ece60d186575517331301e832ec4
steeplejack <kcgori1982@gmail.com>
2024-12-07 12:51:23 +0000

    Solved using recursion in python with memoisation. A bit slow on part2.

---
+++ Day7/part2.py
@@ -0,0 +1,81 @@
+import functools
+
+class Problem:
+    def __init__(self, target_string, component_string):
+        self.target = int(target_string)
+        self.components = tuple(int(x) for x in component_string.strip().split())
+
+    def __repr__(self):
+        return f'Problem({self.target}, {self.components})'
+
+    def __len__(self):
+        return len(self.components)
+
+    def simple_solve(self):
+        return sum(self.components) == self.target
+
+    def solve(self):
+        return solve(self.target, self.components[1:], self.components[0], '')
+
+@functools.cache
+def solve(target, components, accumulator, operations):
+    # print(target, components, accumulator, operations)
+    if len(components) == 0:
+        return accumulator == target, accumulator, operations
+
+    if accumulator > target:
+        return False, accumulator, 'exceed'
+    
+    prod_arm = solve(target,
+                 components[1:],
+                 accumulator * components[0],
+                 operations + '*')
+    
+    sum_arm = solve(target,
+                 components[1:],
+                 accumulator + components[0],
+                 operations + '+')
+
+    acc = int(str(accumulator) + str(components[0]))
+    concat_arm = solve(target,
+                       components[1:],
+                       acc,
+                       operations + '|')
+
+    if prod_arm[0]:
+        return prod_arm 
+
+    if sum_arm[0]:
+        return sum_arm
+
+    if concat_arm[0]:
+        return concat_arm
+
+    return False, accumulator, 'fail'
+        
+
+def read_input(filename):
+    problems = []
+    with open(filename) as fl:
+        for line in fl:
+            target, components = line.strip().split(':')
+            problems.append(Problem(target, components))
+    return problems
+
+def main():
+    problems = read_input('input.txt')
+
+    acc = 0
+    for prob in problems:
+        if prob.simple_solve():
+            acc += prob.target
+        else:
+            soln = prob.solve()
+            if soln[0]:
+                acc += prob.target
+    print(f'Part 1: {acc}')
+    return acc
+
+if __name__ == '__main__':
+    result = main()
+
