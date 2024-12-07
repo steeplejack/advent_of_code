f5b7ed97aa9818bd8bd3df65b74117a7165ccd96
steeplejack <kcgori1982@gmail.com>
2024-12-07 14:32:08 +0000

    Removed all unnecessary elements to speed up recursion

--- Day7/part2.py
+++ Day7/part2.py
@@ -1,5 +1,3 @@
-import functools
-
 class Problem:
     def __init__(self, target_string, component_string):
         self.target = int(target_string)
@@ -8,60 +6,21 @@ class Problem:
     def __repr__(self):
         return f'Problem({self.target}, {self.components})'
 
-    def __len__(self):
-        return len(self.components)
+def solve(target, components):
+    def inner(index, accumulator):
+        if accumulator > target:
+            return False
+        
+        if index == l:
+            return accumulator == target
 
-    def simple_sum(self):
-        return sum(self.components) == self.target
+        return inner(index + 1, accumulator * components[index]) or \
+            inner(index + 1, accumulator + components[index]) or \
+            inner(index + 1, int(str(accumulator) + str_components[index]))
 
-    def simple_prod(self):
-        acc = self.components[0]
-        for x in self.components[1:]:
-            acc *= x
-        return acc == self.target
-
-    def simple_concat(self):
-        return ''.join(str(x) for x in self.components) == str(self.target)
-
-    def solve(self):
-        return solve(self.target, self.components[1:], self.components[0], '')
-
-@functools.cache
-def solve(target, components, accumulator, operations):
-    # print(target, components, accumulator, operations)
-    if len(components) == 0:
-        return accumulator == target, accumulator, operations
-
-    if accumulator > target:
-        return False, accumulator, 'exceed'
-
-    prod_arm = solve(target,
-                 components[1:],
-                 accumulator * components[0],
-                 operations + '*')
-
-    if prod_arm[0]:
-        return prod_arm
-
-    sum_arm = solve(target,
-                 components[1:],
-                 accumulator + components[0],
-                 operations + '+')
-
-    if sum_arm[0]:
-        return sum_arm
-
-    acc = int(str(accumulator) + str(components[0]))
-    concat_arm = solve(target,
-                       components[1:],
-                       acc,
-                       operations + '|')
-
-    if concat_arm[0]:
-        return concat_arm
-
-    return False, accumulator, 'fail'
-
+    l = len(components)
+    str_components = [str(x) for x in components]
+    return inner(1, components[0])
 
 def read_input(filename):
     problems = []
@@ -76,14 +35,11 @@ def main():
 
     acc = 0
     for prob in problems:
-        if prob.simple_sum() or prob.simple_prod() or prob.simple_concat():
+        if solve(prob.target, prob.components):
             acc += prob.target
-        else:
-            soln = prob.solve()
-            if soln[0]:
-                acc += prob.target
     return acc
 
 if __name__ == '__main__':
     result = main()
+    assert result == 249943041417600 or result == 11387, 'Wrong answer'
     print(f'Part2: {result}')
