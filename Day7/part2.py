9bef1a8920ab64d1810453985ca62046f6f11034
steeplejack <kcgori1982@gmail.com>
2024-12-07 13:04:22 +0000

    Made it a bit faster by reordering the recursion

--- Day7/part2.py
+++ Day7/part2.py
@@ -11,8 +11,17 @@ class Problem:
     def __len__(self):
         return len(self.components)
 
-    def simple_solve(self):
+    def simple_sum(self):
         return sum(self.components) == self.target
+
+    def simple_prod(self):
+        acc = self.components[0]
+        for x in self.components[1:]:
+            acc *= x
+        return acc == self.target
+
+    def simple_concat(self):
+        return ''.join(str(x) for x in self.components) == str(self.target)
 
     def solve(self):
         return solve(self.target, self.components[1:], self.components[0], '')
@@ -25,16 +34,22 @@ def solve(target, components, accumulator, operations):
 
     if accumulator > target:
         return False, accumulator, 'exceed'
-    
+
     prod_arm = solve(target,
                  components[1:],
                  accumulator * components[0],
                  operations + '*')
-    
+
+    if prod_arm[0]:
+        return prod_arm
+
     sum_arm = solve(target,
                  components[1:],
                  accumulator + components[0],
                  operations + '+')
+
+    if sum_arm[0]:
+        return sum_arm
 
     acc = int(str(accumulator) + str(components[0]))
     concat_arm = solve(target,
@@ -42,17 +57,11 @@ def solve(target, components, accumulator, operations):
                        acc,
                        operations + '|')
 
-    if prod_arm[0]:
-        return prod_arm 
-
-    if sum_arm[0]:
-        return sum_arm
-
     if concat_arm[0]:
         return concat_arm
 
     return False, accumulator, 'fail'
-        
+
 
 def read_input(filename):
     problems = []
@@ -67,15 +76,14 @@ def main():
 
     acc = 0
     for prob in problems:
-        if prob.simple_solve():
+        if prob.simple_sum() or prob.simple_prod() or prob.simple_concat():
             acc += prob.target
         else:
             soln = prob.solve()
             if soln[0]:
                 acc += prob.target
-    print(f'Part 1: {acc}')
     return acc
 
 if __name__ == '__main__':
     result = main()
-
+    print(f'Part2: {result}')
