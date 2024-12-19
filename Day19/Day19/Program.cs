74ab85c7c8465cf23f8c973ff8ae35a7ad60ccfa
steeplejack <kcgori1982@gmail.com>
2024-12-19 12:12:18 +0000

    Solved Day 19 Part 1 using dynamic programming. Using memoisation is essential.

---
+++ Day19/Day19/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Day19;
+
+class Program
+{
+    static (List<string>, List<string>) ReadInput(string filename)
+    {
+        List<String> patterns;
+        List<string> designs = new List<string>();
+        using (StreamReader reader = File.OpenText(filename))
+        {
+            string line;
+            patterns = reader.ReadLine().Trim().Split(',').Select(x => x.Trim()).ToList();
+            line = reader.ReadLine();
+            while ((line = reader.ReadLine()) != null)
+            {
+                designs.Add(line.Trim());
+            }
+        }
+
+        return (patterns, designs);
+    }
+
+    static bool DynProg(List<string> patterns, string design, Dictionary<string, bool> cache)
+    {
+        if (cache.ContainsKey(design))
+        {
+            return cache[design];
+        }
+
+        foreach (var pattern in patterns)
+        {
+            if (pattern == design)
+            {
+                cache[design] = true;
+                return true;
+            }
+
+            if (design.StartsWith(pattern))
+            {
+                if (DynProg(patterns, design.Substring(pattern.Length), cache))
+                {
+                    cache[design] = true;
+                    return true;
+                }
+            }
+        }
+        cache[design] = false;
+        return false;
+    }
+
+    static int Part1(List<string> patterns, List<string> designs)
+    {
+        return designs
+            .Select<string, bool>(design =>
+            {
+                return DynProg(patterns, design, new Dictionary<string, bool>());
+            })
+            .Select(x => x ? 1 : 0)
+            .Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        var (patterns, designs) = ReadInput(args[1]);
+        int part1 = Part1(patterns, designs);
+        Console.WriteLine($"Part 1: {part1}");
+    }
+}
