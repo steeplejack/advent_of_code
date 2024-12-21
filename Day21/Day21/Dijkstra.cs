e269bee1b5af3a36b058d7bf4cf0f38677b6b8df
steeplejack <kcgori1982@gmail.com>
2024-12-21 18:14:52 +0000

    Day 21 Part 1. This solves it, but it's not going to work for part 2...

---
+++ Day21/Day21/Dijkstra.cs
@@ -0,0 +1,329 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Day21;
+
+public class Node<T>
+{
+    public T Value { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"Node({Value.ToString()})";
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Node<T> node && EqualityComparer<T>.Default.Equals(Value, node.Value);
+    }
+
+    public bool Equals(Node<T>? other)
+    {
+        return EqualityComparer<T>.Default.Equals(Value, other.Value);
+    }
+
+    public override int GetHashCode()
+    {
+        return EqualityComparer<T>.Default.GetHashCode(Value);
+    }
+
+    public static bool operator ==(Node<T> left, Node<T>? right)
+    {
+        if (left is null && right is null) { return true; }
+        if (left is null || right is null) { return false; }
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Node<T> left, Node<T> right)
+    {
+        return !(left == right);
+    }
+}
+
+public class Graph<T> where T : notnull
+{
+    public Dictionary<T, Node<T>> Nodes { get; }
+    public Dictionary<Node<T>, Dictionary<Node<T>, double>> Edges { get; }
+
+    public Graph()
+    {
+        Nodes = new Dictionary<T, Node<T>>();
+        Edges = new Dictionary<Node<T>, Dictionary<Node<T>, double>>();
+    }
+
+    public void AddNode(T value)
+    {
+        if (Nodes.ContainsKey(value))
+        {
+            throw new ArgumentException($"Node already exists: {value}");
+        }
+        Nodes[value] = new Node<T>(value);
+    }
+
+    public Node<T>[] GetNodes()
+    {
+        return Nodes.Values.ToArray();
+    }
+
+    public Node<T> GetNode(T value)
+    {
+        if (Nodes.TryGetValue(value, out var node))
+        {
+            return node;
+        }
+        else
+        {
+            throw new ArgumentException($"Node not found: {value}");
+        }
+    }
+
+    public void AddEdge(T head, T tail, double distance)
+    {
+        var head_node = GetNode(head);
+        var tail_node = GetNode(tail);
+
+        if (!Edges.ContainsKey(head_node))
+        {
+            Edges[head_node] = new Dictionary<Node<T>, double>();
+        }
+        Edges[head_node][tail_node] = distance;
+    }
+
+    public List<(Node<T>, double)> GetNeighbours(Node<T> node)
+    {
+        var neighbours = new List<(Node<T>, double)>();
+        if (!Edges.ContainsKey(node)) { return neighbours; }
+        foreach (var kvp in Edges[node])
+        {
+            neighbours.Add((kvp.Key, kvp.Value));
+        }
+        return neighbours;
+    }
+
+    public Dictionary<Node<T>, double> Dijkstra(Node<T> start)
+    {
+        if (!Nodes.Values.Contains(start))
+        {
+            throw new ArgumentException($"Start node not found: {start}");
+        }
+        PriorityQueue<(Node<T>, double), double> queue = new PriorityQueue<(Node<T>, double), double>();
+        queue.Enqueue((start, 0), 0);
+
+        var distances = new Dictionary<Node<T>, double>();
+        foreach (var node in GetNodes())
+        {
+            distances[node] = double.PositiveInfinity;
+        }
+        distances[start] = 0;
+
+        while (queue.Count > 0)
+        {
+            var (currentNode, currentDist) = queue.Dequeue();
+            if (currentDist > distances[currentNode]) { continue; }
+
+            foreach (var (neighbour, dist) in GetNeighbours(currentNode))
+            {
+                double new_dist = dist + currentDist;
+                if (new_dist < distances[neighbour])
+                {
+                    distances[neighbour] = new_dist;
+                    queue.Enqueue((neighbour, new_dist), new_dist);
+                }
+            }
+        }
+
+        return distances;
+    }
+
+
+    public double Dijkstra(Node<T> start, Node<T> end)
+    {
+        if (!Nodes.Values.Contains(start))
+        {
+            throw new ArgumentException($"Start node not found: {start}");
+        }
+        if (!Nodes.Values.Contains(end))
+        {
+            throw new ArgumentException($"Node not found: {end}");
+        }
+
+        PriorityQueue<(Node<T>, double), double> queue = new PriorityQueue<(Node<T>, double), double>();
+        queue.Enqueue((start, 0), 0);
+
+        var distances = new Dictionary<Node<T>, double>();
+        foreach (var node in GetNodes())
+        {
+            distances[node] = double.PositiveInfinity;
+        }
+        distances[start] = 0;
+
+        while (queue.Count > 0)
+        {
+            var (currentNode, currentDist) = queue.Dequeue();
+            if (currentNode == end) { return distances[end]; }
+            if (currentDist > distances[currentNode]) { continue; }
+
+            foreach (var (neighbour, dist) in GetNeighbours(currentNode))
+            {
+                double new_dist = dist + currentDist;
+                if (new_dist < distances[neighbour])
+                {
+                    distances[neighbour] = new_dist;
+                    queue.Enqueue((neighbour, new_dist), new_dist);
+                }
+            }
+        }
+
+        return distances[end];
+    }
+
+    public (Dictionary<Node<T>, double>, Dictionary<Node<T>, List<List<Node<T>>>>) DijkstraPath(Node<T> start)
+    {
+        if (!Nodes.Values.Contains(start))
+        {
+            throw new ArgumentException($"Start node not found: {start}");
+        }
+
+        PriorityQueue<(Node<T>, double), double> queue = new PriorityQueue<(Node<T>, double), double>();
+        queue.Enqueue((start, 0), 0);
+
+        var distances = new Dictionary<Node<T>, double>();
+        foreach (var node in GetNodes())
+        {
+            distances[node] = double.PositiveInfinity;
+        }
+        distances[start] = 0;
+
+        var paths = new Dictionary<Node<T>, List<List<Node<T>>>>();
+        paths[start] = new List<List<Node<T>>>
+        {
+            new List<Node<T>> { start }
+        };
+
+        while (queue.Count > 0)
+        {
+            var (currentNode, currentDist) = queue.Dequeue();
+            if (currentDist > distances[currentNode]) { continue; }
+
+            foreach (var (neighbour, dist) in GetNeighbours(currentNode))
+            {
+                double new_dist = dist + currentDist;
+                if (new_dist < distances[neighbour])
+                {
+                    distances[neighbour] = new_dist;
+                    paths[neighbour] = paths[currentNode]
+                        .Select(path => new List<Node<T>>(path) { neighbour})
+                        .ToList();
+                    queue.Enqueue((neighbour, new_dist), new_dist);
+                }
+
+                else if (new_dist == distances[neighbour])
+                {
+                    paths[neighbour].AddRange(paths[currentNode]
+                        .Select(path => new List<Node<T>>(path) { neighbour }));
+                }
+            }
+        }
+
+        return (distances, paths);
+    }
+
+    public (double, List<List<Node<T>>>) DijkstraPath(Node<T> start, Node<T> end)
+    {
+        if (!Nodes.Values.Contains(start))
+        {
+            throw new ArgumentException($"Start node not found: {start}");
+        }
+        if (!Nodes.Values.Contains(end))
+        {
+            throw new ArgumentException($"Node not found: {end}");
+        }
+
+        PriorityQueue<(Node<T>, double), double> queue = new PriorityQueue<(Node<T>, double), double>();
+        queue.Enqueue((start, 0), 0);
+
+        var distances = new Dictionary<Node<T>, double>();
+        foreach (var node in GetNodes())
+        {
+            distances[node] = double.PositiveInfinity;
+        }
+        distances[start] = 0;
+
+        var paths = new Dictionary<Node<T>, List<List<Node<T>>>>();
+        paths[start] = new List<List<Node<T>>>
+        {
+            new List<Node<T>> { start }
+        };
+
+        while (queue.Count > 0)
+        {
+            var (currentNode, currentDist) = queue.Dequeue();
+            if (currentNode == end) { return (distances[end], paths[end]); }
+            if (currentDist > distances[currentNode]) { continue; }
+
+            foreach (var (neighbour, dist) in GetNeighbours(currentNode))
+            {
+                double new_dist = dist + currentDist;
+                if (new_dist < distances[neighbour])
+                {
+                    distances[neighbour] = new_dist;
+                    paths[neighbour] = paths[currentNode]
+                        .Select(path => new List<Node<T>>(path) { neighbour})
+                        .ToList();
+                    queue.Enqueue((neighbour, new_dist), new_dist);
+                }
+
+                else if (new_dist == distances[neighbour])
+                {
+                    paths[neighbour].AddRange(paths[currentNode]
+                        .Select(path => new List<Node<T>>(path) { neighbour }));
+                }
+            }
+        }
+
+        return (distances[end], paths[end]);
+    }
+}
+
+class TestDijkstra
+{
+    static Graph<char> BuildTestGraph()
+    {
+        var graph = new Graph<char>();
+        foreach (char c in "ABCDEFGHI")
+        {
+            graph.AddNode(c);
+        }
+
+        graph.AddEdge('A', 'B', 12);
+        graph.AddEdge('A', 'C', 8);
+        graph.AddEdge('A', 'D', 11);
+        graph.AddEdge('B', 'E', 19);
+        graph.AddEdge('C', 'D', 6);
+        graph.AddEdge('C', 'E', 7);
+        graph.AddEdge('D', 'F', 8);
+        graph.AddEdge('D', 'H', 2);
+        graph.AddEdge('D', 'I', 15);
+        graph.AddEdge('E', 'F', 5);
+        graph.AddEdge('F', 'G', 6);
+        graph.AddEdge('H', 'G', 1);
+        graph.AddEdge('H', 'I', 11);
+
+        return graph;
+    }
+
+    static bool Test()
+    {
+        var graph = BuildTestGraph();
+        var start = graph.GetNode('A');
+        var end = graph.GetNode('G');
+
+        var d= graph.Dijkstra(start, end);
+        return d == 14.0;
+    }
+}
---
+++ Day21/Day21/Program.cs
@@ -0,0 +1,202 @@
+﻿namespace Day21;
+
+class DirPad : KeyPad
+{
+    protected override (int, int, char)[] _nodeValues => new (int, int, char)[]
+    {
+        (0, 1, '^'), (0, 2, 'A'), (1, 0, '<'), (1, 1, 'v'), (1, 2, '>')
+    };
+}
+
+class NumPad : KeyPad
+{
+    protected override (int, int, char)[] _nodeValues => new (int, int, char)[]
+    {
+        (0,0,'7'),
+        (0,1,'8'),
+        (0,2,'9'),
+        (1,0,'4'),
+        (1,1,'5'),
+        (1,2,'6'),
+        (2,0,'1'),
+        (2,1,'2'),
+        (2,2,'3'),
+        (3,1,'0'),
+        (3,2,'A')
+    };
+}
+
+class KeyPad
+{
+    protected virtual (int, int, char)[] _nodeValues => new (int, int, char)[]
+    {
+        (0,0,'7'),
+        (0,1,'8'),
+        (0,2,'9'),
+        (1,0,'4'),
+        (1,1,'5'),
+        (1,2,'6'),
+        (2,0,'1'),
+        (2,1,'2'),
+        (2,2,'3'),
+        (3,1,'0'),
+        (3,2,'A')
+    };
+
+    public Graph<(int,int,char)> Graph { get; }
+
+    private Dictionary<(char, char), List<List<Node<(int, int, char)>>>> _paths = new();
+
+    public KeyPad()
+    {
+        // initialise the graph
+        Graph = new();
+
+        // add nodes to the graph
+        foreach (var node in _nodeValues)
+        {
+            Graph.AddNode(node);
+        }
+
+        // add edges to the graph
+        foreach (var source in _nodeValues)
+        {
+            foreach (var dest in _nodeValues)
+            {
+                if (source == dest)
+                {
+                    continue;
+                }
+
+                if (Math.Abs(source.Item1 - dest.Item1) + Math.Abs(source.Item2 - dest.Item2) ==
+                    1)
+                {
+                    Graph.AddEdge(source, dest, 1.0);
+                }
+            }
+        }
+
+        // collect paths
+        foreach (var source in _nodeValues)
+        {
+            var sourceNode = Graph.GetNode(source);
+            foreach (var dest in _nodeValues)
+            {
+                var destNode = Graph.GetNode(dest);
+                var (_, paths) = Graph.DijkstraPath(sourceNode, destNode);
+                _paths.Add((source.Item3, dest.Item3), paths);
+            }
+        }
+    }
+
+    public Graph<(int, int, char)> GetGraph() => Graph;
+    public Dictionary<(char, char), List<List<Node<(int, int, char)>>>> GetPaths() => _paths;
+
+    public static string PathToString(List<Node<(int, int, char)>> path)
+    {
+        if (path.Count == 0)
+        {
+            return "";
+        }
+
+        if (path.Count == 1)
+        {
+            return "A";
+        }
+
+        var output = "";
+        for (int i = 1; i < path.Count; i++)
+        {
+            var from = path[i - 1];
+            var to = path[i];
+            var vDiff = to.Value.Item1 - from.Value.Item1;
+            var hDiff = to.Value.Item2 - from.Value.Item2;
+            output += (vDiff, hDiff) switch
+            {
+                (1, 0) => "v",
+                (-1, 0) => "^",
+                (0, 1) => ">",
+                (0, -1) => "<",
+                _ => throw new InvalidOperationException("Invalid path"),
+            };
+
+        }
+
+        return output + "A";
+    }
+}
+
+
+class Program
+{
+    static string[] ReadInput(string filename)
+    {
+        using (var reader = File.OpenText(filename))
+        {
+            return reader.ReadToEnd().Split('\n');
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var input = ReadInput(args[1]);
+        foreach (var line in input)
+        {
+            Console.WriteLine(line);
+        }
+
+        var numpad = new NumPad();
+        var numpadPaths = numpad.GetPaths();
+
+        var dirpad = new DirPad();
+        var dirpadPaths = dirpad.GetPaths();
+
+        long s = 0;
+
+        foreach (var i in input)
+        {
+            if (i.Length == 0) continue;
+            int n = int.Parse(i.Substring(0, 3));
+            var shortestPaths1 = ShortestPaths(i, numpadPaths);
+            var shortestPaths2 = shortestPaths1.SelectMany(p => ShortestPaths(p, dirpadPaths)).ToList();
+            var shortestPaths3 = shortestPaths2.SelectMany(p => ShortestPaths(p, dirpadPaths)).Min(s => s.Length);
+
+            s += (long)(shortestPaths3 * n);
+        }
+        Console.WriteLine(s);
+    }
+
+    static List<string> ShortestPaths(string sequence, Dictionary<(char, char), List<List<Node<(int, int, char)>>>> paths)
+    {
+        return Rec(("A" + sequence).ToCharArray(), 1, paths, "", new Dictionary<(char[], int), List<string>>());
+    }
+
+    static List<string> Rec(char[] sequence, int index, Dictionary<(char, char), List<List<Node<(int, int, char)>>>> paths, string acc, Dictionary<(char[], int), List<string>> cache)
+    {
+        if (cache.ContainsKey((sequence, index)))
+        {
+            return cache[(sequence, index)];
+        }
+        List<string> output = new();
+        if (index == sequence.Length - 1)
+        {
+            foreach (var path in paths[(sequence[index - 1], sequence[index])])
+            {
+                output.Add(acc + KeyPad.PathToString(path));
+            }
+
+            cache.Add((sequence, index), output);
+            return output;
+        }
+        else
+        {
+            foreach (var path in paths[(sequence[index - 1], sequence[index])])
+            {
+                output.AddRange(Rec(sequence, index + 1, paths, acc + KeyPad.PathToString(path), cache));
+            }
+
+            cache.Add((sequence, index), output);
+            return output;
+        }
+    }
+}
